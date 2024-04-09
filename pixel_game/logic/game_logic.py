f91442327fa5d70d4e2257f1cd74d449ea4502ae
nikogamsahurdia <eleron96@gmail.com>
2024-04-09 19:05:10 +0300

    pixel has updated

--- pixel_game/logic/game_logic.py
+++ pixel_game/logic/game_logic.py
@@ -5,6 +5,7 @@ import math
 from ..menu.settings import load_settings
 import asyncio
 from .walls import add_walls
+from .pixel import Pixel
 
 
 
@@ -18,63 +19,6 @@ attract_force = settings.get("attract_force", 1000)
 font_size = settings.get("font_size", 16)
 pixel_size = settings.get("pixel_size", 10)
 
-
-
-def generate_angles(split_parts):
-    for i in range(split_parts):
-        yield 2 * math.pi * i / split_parts
-
-
-class Pixel:
-    def __init__(self, space, x, y, size, speed_x, speed_y, split_parts):
-        self.space = space
-        self.size = size
-        self.split_parts = split_parts
-        mass = 1
-        moment = pymunk.moment_for_box(mass, (size, size))
-        self.body = pymunk.Body(mass, moment)
-        self.body.position = x, y
-        self.body.velocity = speed_x, speed_y
-        self.shape = pymunk.Poly.create_box(self.body, (size, size))
-        self.shape.elasticity = elasticity
-        self.shape.friction = friction
-        self.space.add(self.body, self.shape)
-
-    def draw(self, screen, speed):
-        x, y = self.body.position
-        color = calculate_color(speed)
-        pygame.draw.rect(screen, pygame.Color(color[0], color[1], color[2]), (x, y, self.size, self.size))
-
-    def split(self):
-        if self.size <= 2:
-            self.space.remove(self.body, self.shape)
-            return []
-
-        new_size = max(self.size // 2, 1)
-        new_pixels = []
-        base_speed = math.sqrt(
-            self.body.velocity.x ** 2 + self.body.velocity.y ** 2) * 1.4
-
-        for angle in generate_angles(self.split_parts):
-            new_speed_x = base_speed * math.cos(angle)
-            new_speed_y = base_speed * math.sin(angle)
-            new_x, new_y = self.body.position
-            new_pixels.append(
-                Pixel(self.space, new_x, new_y, new_size, new_speed_x,
-                      new_speed_y, self.split_parts))
-
-        return new_pixels
-
-    def apply_force(self, pos):
-        direction = pymunk.Vec2d(pos[0], pos[1]) - self.body.position
-        self.body.apply_force_at_local_point(direction.normalized() * attract_force, (0, 0))
-
-
-def calculate_color(speed):
-    red = min(max(min_speed_color[0] + (max_speed_color[0] - min_speed_color[0]) * (100 - speed) / 90, 0), 255)
-    green = min(max(min_speed_color[1] + (max_speed_color[1] - min_speed_color[1]) * (100 - speed) / 90, 0), 255)
-    blue = min(max(min_speed_color[2] + (max_speed_color[2] - min_speed_color[2]) * (100 - speed) / 90, 0), 255)
-    return int(red), int(green), int(blue)
 
 
 async def check_pixel_position(pixel, screen_width, screen_height):
