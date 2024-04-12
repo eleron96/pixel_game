c9c7e75b3228fd0c08016cdd3f139c0dfb1b8ab4
nikogamsahurdia <eleron96@gmail.com>
2024-04-12 18:32:50 +0300

    minor bug fixed

--- pixel_game/logic/Pixel.py
+++ pixel_game/logic/Pixel.py
@@ -3,20 +3,29 @@ import math
 import pygame
 from ..menu.settings import load_settings
 
-
 settings = load_settings()
+
+
 def calculate_color(speed):
     settings = load_settings()
     min_speed_color = settings.get("min_speed_color", [255, 255, 255])
     max_speed_color = settings.get("max_speed_color", [173, 232, 244])
-    red = min(max(min_speed_color[0] + (max_speed_color[0] - min_speed_color[0]) * (100 - speed) / 90, 0), 255)
-    green = min(max(min_speed_color[1] + (max_speed_color[1] - min_speed_color[1]) * (100 - speed) / 90, 0), 255)
-    blue = min(max(min_speed_color[2] + (max_speed_color[2] - min_speed_color[2]) * (100 - speed) / 90, 0), 255)
+    red = min(max(
+        min_speed_color[0] + (max_speed_color[0] - min_speed_color[0]) * (
+                    100 - speed) / 90, 0), 255)
+    green = min(max(
+        min_speed_color[1] + (max_speed_color[1] - min_speed_color[1]) * (
+                    100 - speed) / 90, 0), 255)
+    blue = min(max(
+        min_speed_color[2] + (max_speed_color[2] - min_speed_color[2]) * (
+                    100 - speed) / 90, 0), 255)
     return int(red), int(green), int(blue)
+
 
 def generate_angles(split_parts):
     for i in range(split_parts):
         yield 2 * math.pi * i / split_parts
+
 
 class Pixel:
     def __init__(self, space, x, y, size, speed_x, speed_y, split_parts):
@@ -36,7 +45,8 @@ class Pixel:
     def draw(self, screen, speed):
         x, y = self.body.position
         color = calculate_color(speed)
-        pygame.draw.rect(screen, pygame.Color(color[0], color[1], color[2]), (x, y, self.size, self.size))
+        pygame.draw.rect(screen, pygame.Color(color[0], color[1], color[2]),
+                         (x, y, self.size, self.size))
 
     def split(self):
         if self.size <= 2:
@@ -45,16 +55,21 @@ class Pixel:
 
         new_size = max(self.size // 2, 1)
         new_pixels = []
-        base_speed = math.sqrt(self.body.velocity.x ** 2 + self.body.velocity.y ** 2) * 1.4
+        base_speed = math.sqrt(
+            self.body.velocity.x ** 2 + self.body.velocity.y ** 2) * 1.4
 
         for angle in generate_angles(self.split_parts):
             new_speed_x = base_speed * math.cos(angle)
             new_speed_y = base_speed * math.sin(angle)
             new_x, new_y = self.body.position
-            new_pixels.append(Pixel(self.space, new_x, new_y, new_size, new_speed_x, new_speed_y, self.split_parts))
+            new_pixels.append(
+                Pixel(self.space, new_x, new_y, new_size, new_speed_x,
+                      new_speed_y, self.split_parts))
 
         return new_pixels
 
     def apply_force(self, pos):
         direction = pymunk.Vec2d(pos[0], pos[1]) - self.body.position
-        self.body.apply_force_at_local_point(direction.normalized() * settings.get("attract_force", 1000), (0, 0))
+        self.body.apply_force_at_local_point(
+            direction.normalized() * settings.get("attract_force", 1000),
+            (0, 0))
--- pixel_game/logic/game_logic.py
+++ pixel_game/logic/game_logic.py
@@ -7,7 +7,6 @@ from .walls import add_walls
 from .pixel import Pixel
 
 
-
 settings = load_settings()
 wall_thickness = settings.get("wall_thickness", 5)
 elasticity = settings.get("elasticity", 1.0)
@@ -17,6 +16,7 @@ max_speed_color = settings.get("max_speed_color", [173, 232, 244])
 attract_force = settings.get("attract_force", 1000)
 pixel_size = settings.get("pixel_size", 10)
 
+
 def run_game():
     pygame.init()
     screen_width, screen_height = 320, 240
@@ -25,14 +25,17 @@ def run_game():
     space = pymunk.Space()
     space.gravity = (0, 20)
 
-    add_walls(space, screen_width, screen_height, wall_thickness, elasticity, friction)
+    add_walls(space, screen_width, screen_height, wall_thickness, elasticity,
+              friction)
 
     settings = load_settings()
     initial_pixel_size = settings.get("initial_pixel_size", 16)
     pixel_split_parts = settings.get("pixel_split_parts", 4)
     font_size = settings.get("font_size", 16)
 
-    pixels = [Pixel(space, screen_width // 2, screen_height // 2, initial_pixel_size, 100, -100, pixel_split_parts)]
+    pixels = [
+        Pixel(space, screen_width // 2, screen_height // 2, initial_pixel_size,
+              100, -100, pixel_split_parts)]
     attraction_force = 0
     font = pygame.font.Font(None, font_size)
 
@@ -62,7 +65,7 @@ def run_game():
                 pixel.body.apply_force_at_local_point(direction_vector)
 
         screen.fill((0, 0, 0))
-        space.step(1 / 120.0)  # Step the simulation forward
+        space.step(1 / 40.0)  # Step the simulation forward
 
         cursor_pos = pygame.mouse.get_pos()
         new_pixels = []
@@ -76,7 +79,7 @@ def run_game():
                 pixels.remove(pixel)
             else:
                 d = math.sqrt((pixel.body.position.x - cursor_pos[0]) ** 2 + (
-                            pixel.body.position.y - cursor_pos[1]) ** 2)
+                        pixel.body.position.y - cursor_pos[1]) ** 2)
                 if d <= pixel.size:
                     # Разделяем пиксель и получаем новые пиксели
                     new_pixels = pixel.split()
@@ -90,19 +93,23 @@ def run_game():
 
         pixels.extend(new_pixels)
 
-        pixel_count_text = font.render(f"pixels: {len(pixels)}", True, (255, 255, 255))
+        pixel_count_text = font.render(f"pixels: {len(pixels)}", True,
+                                       (255, 255, 255))
         screen.blit(pixel_count_text, (10, 10))
-        fps_text = font.render(f"FPS: {clock.get_fps():.2f}", True, (255, 255, 255))
+        fps_text = font.render(f"FPS: {clock.get_fps():.2f}", True,
+                               (255, 255, 255))
         screen.blit(fps_text, (10, 30))
 
         for pixel in pixels:
-            speed = math.sqrt(pixel.body.velocity.x**2 + pixel.body.velocity.y**2)
+            speed = math.sqrt(
+                pixel.body.velocity.x ** 2 + pixel.body.velocity.y ** 2)
             pixel.draw(screen, speed)
 
         pygame.display.flip()
         clock.tick(120)
 
     return return_to_menu
+
 
 if __name__ == "__main__":
     run_game()
